baed6e2700461d47bad6fe52a50b282e06be43fc
Sam LaGrave <srlagrave@gmail.com>
2023-12-01 22:05:57 -0600

    Another small script change

--- setup_day.sh
+++ setup_day.sh
@@ -1,12 +1,11 @@
 echo "Creating folder day$1"
+echo "- **Day $1**" >> README.md
+echo "  - ☐ Python" >> README.md
+echo "  - ☐ Rust" >> README.md
 mkdir day$1
 cd day$1
 mkdir python
 cp ../python_template.py ./python/main.py
 touch input.txt
 cargo new rust
-cd ..
-echo "- **Day $1**" >> README.md
-echo "  - ☐ Python" >> README.md
-echo "  - ☐ Rust" >> README.md
 code .
