d1e6f3a8f7f077f5a1a3d4d7293559efb8716e2f
Sam LaGrave <38541580+SLaGrave@users.noreply.github.com>
2023-12-06 10:10:04 -0600

    Day 6 - Python done

--- day06/python/main.py
+++ day06/python/main.py
@@ -1,29 +1,31 @@
-def clean_data(data: list) -> list:
-    data = [line.strip() for line in data]
+
+## Test data
+# data = [(7, 9), (15, 40), (30, 200)]
+## Part 1
+# data = [(34, 204), (90, 1713), (89, 1210), (86, 1780)]
+# Part 2
+data = [(34908986, 204171312101780)]
+
+def part1():
+    # wins = [0, 0, 0]
+    # wins = [0, 0, 0, 0]
+    wins = [0]
     # Logic goes here
+    for idx, race in enumerate(data):
+        time = 0
+        while time <= race[0]:
+            dist = (race[0]-time)*time
+            if dist > race[1]:
+                wins[idx] += 1
+            time += 1
 
-    return data
+    # return wins[0] * wins[1] * wins[2]
+    # return wins[0] * wins[1] * wins[2] * wins[3]
+    return wins[0]
 
-
-def part1(data: list):
-    total = 0
-    # Logic goes here
-
-    return total
-
-
-def part2(data: list):
-    total = 0
-    # Logic goes here
-    
-    return total
 
 
 if __name__ == "__main__":
-    with open("../input.txt", "r") as f:
-        data = f.readlines()
-    data = clean_data(data)
-    p1 = part1(data)
+    p1 = part1()
     print(p1)
-    p2 = part2(data)
-    print(p2)
+
